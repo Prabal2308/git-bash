0cb436f3445ba7e15d54106490f7dd1d4021dc60
Prabal2308 <prabalsharma2308@gmail.com>
2020-12-02 23:07:57 -0800

    Initial commit

---
+++ calculator.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    //code for printing hello in five different languages
+    char button;
+    cin>>button;
+
+    switch(button)
+    {
+        case 'a':
+        cout<<"Hello"<<endl;
+        break;
+
+        case 'b':
+        cout<<"Namaste"<<endl;
+        break;
+
+        case 'c':
+        cout<<"Hola"<<endl;
+        break;
+
+        case 'd':
+        cout<<"Salut"<<endl;
+        break;
+
+        case 'e':
+        cout<<"Ciao"<<endl;
+        break;
+
+        default:
+        cout<<"I am still learning more"<<endl;
+        break;
+    }
+    return 0;
+}
